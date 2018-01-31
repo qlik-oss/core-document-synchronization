4e143cc6680d508bff0747d6bcfb714285278176
Mattias Wennmo <wennmo@users.noreply.github.com>
2018-01-31 10:08:46 +0100

    Document synchronization example with two qix engines (#2)

--- README.md
+++ README.md
@@ -1,3 +1,74 @@
 # Doc synchronization
 
-Example on how to use synchronize QIX Engine documents between multiple instances in Qlik Core.
+This document contains an example on how to synchronize documents between multiple QIX Engine instances in Qlik Core.
+The orchestration used in this example is [Kubernetes]( https://kubernetes.io/).
+
+To better visualize the synchronization this example contains two running QIX Engine instances deployed as two separate services.
+This is to ensure that the example uses both QIX engine instances and that sessions are not balanced by Kubernetes built-in load balancer.
+
+## Description
+
+This example will use a [Persistent Volume](https://kubernetes.io/docs/concepts/storage/persistent-volumes/) in `Kubernetes`,
+which will be shared between the two QIX Engine instances. The synchronization between the QIX Engines will be handled by a message
+broker called [RabbitMQ](https://www.rabbitmq.com/). When a document is updated e.g. data has been reloaded or new documents were created,
+events will be placed in a queue that both instances are subscribing to.
+
+## Prerequisites
+
+To be able to run this example you will need to have [Minikube](https://github.com/kubernetes/minikube) or equivalent, and `Kubernetes` installed locally.
+
+Since QIX Engine instances uses `nobody` as running user in the docker container,
+the appropriate permissions must be set on the storage that will be used for the documents.
+Run the following command to create the document storage folder in minikube.
+
+```sh
+minikube ssh 'sudo mkdir -p /data/docs;sudo chmod o+rw /data/docs'
+```
+
+To be able to access some of the docker images used in this example you also need to add a kubernetes secret with your docker credentials.
+
+```sh
+kubectl create secret docker-registry dockerhub --docker-username=<your-name> --docker-password=<your-password> --docker-email=<your-email>
+```
+
+## Deploying the example
+
+Deploy the example by running the following command:
+
+```sh
+kubectl create -f example
+```
+
+And to remove the deployment:
+
+```sh
+kubectl delete -f example
+```
+
+## Testing
+
+There is a small scenario implemented using [enigma.js](https://github.com/qlik-oss/enigma.js/) that verifies that the documents are
+synchronized between the two QIX Engine instances. The scenario contains the following steps:
+
+1. Create a document in the first QIX Engine instance.
+1. Open the previously created document in the second QIX Engine instance.
+1. Set a script and perform a reload in the second QIX Engine instance.
+1. Open a session to the document again in the first QIX Engine and verify reload time.
+
+To execute the test locally:
+
+```sh
+cd test
+npm install
+ENGINE1=<URL to QIX Engine> ENGINE2=<URL to QIX Engine> npm run test
+```
+
+You can retrieve the url to an engine by running `minikube service engine --url`. The service name used for the query is specified in the `*-service.yaml`.
+
+The scenario is also part of the Circle CI pipeline and executed for each commit to this repo. For more details look at Circle CI [config](./.circleci/config.yml) file.
+
+## Removing the deployment
+
+```sh
+kubectl delete -f ./core
+```
---
+++ test/test/qix-create-reload-doc.spec.js
@@ -0,0 +1,119 @@
+const WebSocket = require('ws');
+const enigma = require('enigma.js');
+const schema = require('enigma.js/schemas/12.20.0.json');
+
+const script = `
+Characters:
+Load Chr(RecNo()+Ord('A')-1) as Alpha, RecNo() as Num autogenerate 26;
+
+ASCII:
+Load
+if(RecNo()>=65 and RecNo()<=90,RecNo()-64) as Num,
+  Chr(RecNo()) as AsciiAlpha,
+RecNo() as AsciiNum
+autogenerate 255
+Where (RecNo()>=32 and RecNo()<=126) or RecNo()>=160 ;
+
+Transactions:
+Load
+TransLineID,
+TransID,
+mod(TransID,26)+1 as Num,
+Pick(Ceil(3*Rand1),'A','B','C') as Dim1,
+Pick(Ceil(6*Rand1),'a','b','c','d','e','f') as Dim2,
+Pick(Ceil(3*Rand()),'X','Y','Z') as Dim3,
+Round(1000*Rand()*Rand()*Rand1) as Expression1,
+Round(  10*Rand()*Rand()*Rand1) as Expression2,
+Round(Rand()*Rand1,0.00001) as Expression3;
+Load
+Rand() as Rand1,
+IterNo() as TransLineID,
+RecNo() as TransID
+Autogenerate 1000
+While Rand()<=0.5 or IterNo()=1;
+
+Comment Field Dim1 With "This is a field comment";
+`;
+
+describe('QIX doc sync - reload doc', () => {
+  let engine1;
+  let engine2;
+
+  before(() => {
+    if (!process.env.ENGINE1 || !process.env.ENGINE2) {
+      console.log('Please specify engines to use for the test. Check README for instructions.');
+      process.exit(1);
+    }
+    // Remove protocol from url if included
+    engine1 = process.env.ENGINE1.replace(/^(http[s]?:\/\/)/, '');
+    engine2 = process.env.ENGINE2.replace(/^(http[s]?:\/\/)/, '');
+    console.log(`Using engines ${engine1} and ${engine2} in test`);
+  });
+
+  it('should be possible to create a doc in one engine and reload in another', async () => {
+    // Open a session to first engine and create an app
+    console.log('Opening a session to first qix-engine');
+    const session = enigma.create({
+      schema,
+      url: `ws://${engine1}/app/engineData/identity/${+new Date()}`,
+      createSocket: url => new WebSocket(url),
+    });
+
+    const qix1 = await session.open();
+    console.log('Creating doc');
+    const result = await qix1.createApp('testApp');
+    expect(result.qSuccess).to.equal(true);
+    console.log(`Created doc with id: ${result.qAppId}`);
+    const appId = result.qAppId;
+
+    // Open doc and save the reload time
+    let app = await qix1.openDoc(appId);
+    let appLayout = await app.getAppLayout();
+    const firstReloadTime = appLayout.qLastReloadTime;
+    console.log(`Doc was reloaded last time: ${firstReloadTime}`);
+    await session.close();
+    console.log('Closed session to first qix-engine');
+
+    // Open the document in second engine, set script and do reload
+    console.log('Opening a session to second qix-engine and reloading doc');
+    const session2 = enigma.create({
+      schema,
+      url: `ws://${engine2}/app/engineData/identity/${+new Date()}`,
+      createSocket: url => new WebSocket(url),
+    });
+
+    const qix2 = await session2.open();
+    app = await qix2.openDoc(appId);
+    appLayout = await app.getAppLayout();
+    const reloadTime = appLayout.qLastReloadTime;
+    console.log(`Doc was reloaded last time: ${reloadTime}`);
+    expect(reloadTime).to.equal(firstReloadTime);
+
+    await app.setScript(script);
+    await app.doReload(0, false, false);
+    await app.doSave();
+
+    appLayout = await app.getAppLayout();
+    const newReloadTime = appLayout.qLastReloadTime;
+    console.log(`Reload time after reload: ${newReloadTime}`);
+    expect(newReloadTime).to.not.equal(firstReloadTime);
+    await session2.close();
+    console.log('Closed session to second qix-engine');
+
+    // Open doc in first engine again and verify reload time
+    console.log('Opening a session to first qix-engine and verifying reload time');
+    const session3 = enigma.create({
+      schema,
+      url: `ws://${engine1}/app/engineData/identity/${+new Date()}`,
+      createSocket: url => new WebSocket(url),
+    });
+
+    const qix3 = await session3.open();
+    app = await qix3.openDoc(appId);
+    appLayout = await app.getAppLayout();
+    console.log(`Doc was reloaded last time: ${appLayout.qLastReloadTime}`);
+    expect(appLayout.qLastReloadTime).to.equal(newReloadTime);
+    await session3.close();
+    console.log('Closed session to first qix-engine');
+  });
+});
